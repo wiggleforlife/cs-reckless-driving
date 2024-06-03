5c945272be473e1dee2ce56c0d37194eb498bfda
Cian Ormond <co@cianormond.com>
2024-06-03 14:40:47 -0700

    vercel

--- src/index.ts
+++ src/index.ts
@@ -78,3 +78,5 @@ app.post("/api/submit", async (req: Request, res: Response) => {
 app.listen(port, () => {
     console.log(`[server]: Server is running at http://localhost:${port}`);
 });
+
+module.exports = app;
